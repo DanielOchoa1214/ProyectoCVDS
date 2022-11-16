51a9aeedbc95058ee6282d6a0276e66240a749ca
DanielOchoa <danochoa1412@gmail.com>
2022-11-15 21:22:23 -0500

    Datos base de datos agregados, arreglo error de sintaxis en select

--- src/main/java/edu/eci/proyectoCVDS/services/ServiciosRecursoFactory.java
+++ src/main/java/edu/eci/proyectoCVDS/services/ServiciosRecursoFactory.java
@@ -1,11 +1,14 @@
 package edu.eci.proyectoCVDS.services;
 
 import com.google.inject.Injector;
+import edu.eci.proyectoCVDS.entities.TipoRecurso;
 import edu.eci.proyectoCVDS.persistence.DAORecurso;
 import edu.eci.proyectoCVDS.persistence.mybatisimpl.MyBatisDAORecurso;
 import edu.eci.proyectoCVDS.services.impl.ServiciosRecursoImpl;
 import org.mybatis.guice.XMLMyBatisModule;
 import org.mybatis.guice.datasource.helper.JdbcHelper;
+
+import java.time.LocalTime;
 
 import static com.google.inject.Guice.createInjector;
 
@@ -35,6 +38,10 @@ public class ServiciosRecursoFactory {
 
     public static ServiciosRecursoFactory getInstance() {
         return instance;
+    }
+
+    public static void main(String[] args) throws ExcepcionServiciosRecurso {
+        ServiciosRecursoFactory.getInstance().getForumsServices().saveNewResource("Test", "Test", TipoRecurso.Academico, 2, "123", LocalTime.NOON, LocalTime.MIDNIGHT);
     }
 
 }
